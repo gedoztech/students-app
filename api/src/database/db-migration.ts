0b27ee63901d0e07550e764b7d9639d30fe53638
Johnny <johnny00joe@gmail.com>
2021-11-04 11:34:04 -0300

    Configure ES Modules, TypeScript, PostgreSQL connection, DB migrations and nodemon

---
+++ api/src/database/db-migration.ts
@@ -0,0 +1,62 @@
+import { getClient } from './db-connection.js'
+import { listFiles, getFileContents } from '../fs-utils.js'
+
+const migrationsPath = process.cwd() + '/../db/migrations'
+
+async function getOutstandingMigrations (migrations: any[]) {
+  const files: string[] | undefined = await listFiles(migrationsPath)
+  return await Promise.all(
+    (files as string[])
+      .filter((file) => file.split('.')[1] === 'sql')
+      .filter((file) => !migrations.includes(file))
+      .map(async (file) => ({
+        file,
+        query: await getFileContents(file, migrationsPath)
+      }))
+
+  )
+}
+
+async function migrate () {
+  const client = await getClient()
+
+  let existingMigrations = []
+  try {
+    const result = await client.query('SELECT * FROM public.migrations')
+    existingMigrations = result.rows.map(r => r.file)
+  } catch {
+    console.warn('First migration')
+  }
+
+  const outstandingMigrations = await getOutstandingMigrations(
+    existingMigrations
+  )
+
+  if (outstandingMigrations.length > 0) {
+    try {
+      await client.query('BEGIN')
+      for (const migration of outstandingMigrations) {
+        await client.query((migration as { query: { toString: () => any }; file: any }).query.toString())
+        await client.query('INSERT INTO public.migrations (file) VALUES ($1)', [
+          migration.file
+        ])
+      }
+      await client.query('COMMIT')
+      console.log('The migration was performed successfully.')
+      console.log('Wait a moment, the connection is being closed.')
+    } catch (err) {
+      await client.query('ROLLBACK')
+    } finally {
+      client.release()
+    }
+  } else {
+    console.log('No migration to be performed.')
+    console.log('Wait a moment, the connection is being closed.')
+  }
+}
+
+try {
+  migrate()
+} catch (err) {
+  console.log('Error occured while migrating.', err)
+}
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,11 +1,14 @@
 import { createServer } from 'http'
-const HOSTNAME = 'students-api'
-const PORT = process.env.API_DOCKER_PORT
+
+const PORT: number | undefined = parseInt(<string>process.env.API_DOCKER_PORT, 10)
+const HOSTNAME: string = 'students-api'
+
 const server = createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
   res.end('Hello World! I am the students api service.')
 })
+
 server.listen(PORT, HOSTNAME, () => {
   console.log(`Students API server running on port ${PORT}`)
 })
